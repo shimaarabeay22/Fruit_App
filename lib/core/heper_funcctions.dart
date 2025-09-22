0fa748c969ad0a2b5b595b9c084bb3ee629e4e3a
shimaarabeay22 <shimaarabeay2@gmail.com>
2025-09-23 00:22:55 +0300

    first commit

---
+++ lib/core/heper_funcctions.dart
@@ -0,0 +1,21 @@
+
+
+
+
+import 'package:flutter/material.dart';
+import 'package:fruit_app/features/splash/presentation/views/splash_view.dart';
+
+import '../features/on_boarding/presentation/views/on_boarding_view.dart';
+
+Route<dynamic>onGenerateRoute(RouteSettings settings){
+  switch(settings.name){
+    case SplashView.routeName:
+    return MaterialPageRoute(builder: (context)=> const SplashView());
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context)=> const OnBoardingView());
+     default :
+  return MaterialPageRoute(builder: (context)=> const Scaffold());
+
+
+  }
+}
